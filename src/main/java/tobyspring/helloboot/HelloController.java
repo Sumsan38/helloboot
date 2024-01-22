a9b00090487ffe09a80c9e783a7817418d7c389d
Sumsan38 <ndanl4647@gmail.com>
2024-01-22 21:54:09 +0900

    Hello 컨트롤러 매핑과 바인딩

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,13 +1,10 @@
 package tobyspring.helloboot;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
 public class HelloController {
 
-    @GetMapping("/hello")
+
     public String hello(String name){
+        // 아주 간단한 hello 로직
         return "Hello " + name;
     }
 }
--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -18,29 +18,34 @@ public class HellobootApplication {
 
 	public static void main(String[] args) {
 		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-		WebServer webServer = serverFactory.getWebServer(servletContext ->
-				servletContext.addServlet("frontController", new HttpServlet() {
-			@Override
-			protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-				// 프론트 컨트롤러 - 인증, 보안, 다국어, 공통 기능
-				if(req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
-					// "/hello"와 GET 메소드일 경우만 처리
-					String name = req.getParameter("name");
+		WebServer webServer = serverFactory.getWebServer(servletContext -> {
+			HelloController helloController = new HelloController();
 
-					// http 3요소 - 상태 코드, 헤더, 바디
-					resp.setStatus(HttpStatus.OK.value());
-					resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-					resp.getWriter().println("Hello " + name);
-				}
-				else if(req.getRequestURI().equals("/user")) {
-					// 다른 url 처리
-				}
-				else {
-					resp.setStatus(HttpStatus.NOT_FOUND.value());
-				}
+			servletContext.addServlet("frontController", new HttpServlet() {
+					@Override
+					protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+						// 프론트 컨트롤러 - 인증, 보안, 다국어, 공통 기능
+						if(req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
+							// "/hello"와 GET 메소드일 경우만 처리
+							String name = req.getParameter("name");
 
-			}
-		}).addMapping("/*"));	// 슬래시 밑으로 들어오는 모든 요청 등록 -> 이 순간부터 프론트 컨트롤러
+							String ret = helloController.hello(name);	// helloController 안에서 로직이 동작 // "바인딩"
+
+							// http 3요소 - 상태 코드, 헤더, 바디
+							resp.setStatus(HttpStatus.OK.value());
+							resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+							resp.getWriter().println(ret);
+						}
+						else if(req.getRequestURI().equals("/user")) {
+							// 다른 url 처리
+						}
+						else {
+							resp.setStatus(HttpStatus.NOT_FOUND.value());
+						}
+
+					}
+				}).addMapping("/*");
+		});	// 슬래시 밑으로 들어오는 모든 요청 등록 -> 이 순간부터 프론트 컨트롤러
 		webServer.start();
 
 	}
