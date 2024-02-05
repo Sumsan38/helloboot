9b624d5f42a7d4db3995ac0d5aedb67ce2b2f34c
Sumsan38 <ndanl4647@gmail.com>
2024-02-05 22:49:19 +0900

    스프링 컨테이너로 통합

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,14 +1,11 @@
 package tobyspring.helloboot;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Objects;
 
-@RestController // (3)-3. 스프링 부트 3부터는 DispatcherServlet이 Controller까지 기재해야하도록 바뀌었다
-@RequestMapping(value = "/hello")     // (3)-1. 세번째 방법 // DispatcherServlet이 class 레벨에 있는 Mapping 부터 참고한다
+@RestController
 public class HelloController {
 
     private final HelloService helloService;
@@ -17,10 +14,7 @@ public class HelloController {
         this.helloService = helloService;
     }
 
-    /* DispatcherServlet 에 annotation 으로 힌트를 준다 */
-//    @RequestMapping(value = "/hello", method = RequestMethod.GET) // (1) 첫번째 방법
-//    @GetMapping("/hello")       // (2) 두 번째 방법
-    @GetMapping         // (3)-2. 세번째 방법
+    @GetMapping("/hello")
     public String hello(String name){
         return helloService.sayHello(Objects.requireNonNull(name));
     }
--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -9,19 +9,23 @@ import org.springframework.web.servlet.DispatcherServlet;
 public class HellobootApplication {
 
 	public static void main(String[] args) {
-		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
+		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+			@Override
+			protected void onRefresh() {
+				super.onRefresh();
+
+				// 1. dispatcherServlet를 context가 만들어지는 시점에 같이 초기화를 진행하고 싶다
+				ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+				WebServer webServer = serverFactory.getWebServer(servletContext -> {
+					servletContext.addServlet("dispatcherServlet",
+							new DispatcherServlet(this)		// 2. 현재 새로 Override 시도하고 있는 applicationContext 세팅
+					).addMapping("/*");
+				});
+				webServer.start();
+			}
+		};
 		applicationContext.registerBean(HelloController.class);
 		applicationContext.registerBean(SimpleHelloService.class);
 		applicationContext.refresh();
-
-		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-		WebServer webServer = serverFactory.getWebServer(servletContext -> {
-			servletContext.addServlet("dispatcherServlet",
-						// 원래 url과 mehtod 별로 프론트 컨트롤러에 매핑시켜줘야 했었지만, dispatcherServlet + controller에 annotation 기재만으로 코드가 간결해졌다.
-						new DispatcherServlet(applicationContext)		// 그러나 현재는 서블릿에 어떤 요청이 어떻게 들어간다는 힌트가 없기 떄문에, 요청을 보내도 404이 뜬다.
-					).addMapping("/*");
-		});
-		webServer.start();
-
 	}
 }
